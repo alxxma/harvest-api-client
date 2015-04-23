ec0b7935057b36e0dbae72dd5f58f7afb1807829
Alex Maslakov <me@gildedhonour.com>
2015-04-23 19:26:59 +0700

    fixed13

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name='harvest_api_client',
-    version='1.0.19',
+    version='1.0.20',
     description='A client for the Harvest API (getharvest.com)',
     license='MIT',
     author='Alex Maslakov',
